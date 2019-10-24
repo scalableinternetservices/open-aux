1758419a07e4cece2c814362149b86d362b8e390
Han <bin315a1@gmail.com>
2019-10-23 18:13:16 -0700

    ugh

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   #get 'users/new'
-  get '/signup', to:'users#show'
+  get '/signup', to:'users#new'
   post '/signup', to: 'users#create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "application#hello"
