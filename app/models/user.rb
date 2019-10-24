f3708be29b132057672f79d6b88a9464e24ffe8d
David <davidkim0228@gmail.com>
2019-10-24 14:34:44 -0700

    login view + controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,5 @@ class User < ApplicationRecord
     validates :email, presence: true, length: { maximum: 255 },
                       format: { with: VALID_EMAIL_REGEX }, uniqueness: true
     validates :password, presence: true, length: { maximum: 50 }
+    has_secure_password
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   #get 'users/new'
-  get '/signup', to:'users#show'
+  get '/signup', to:'users#new'
   post '/signup', to: 'users#create'
+
+  #login
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "application#hello"
   resources :users
