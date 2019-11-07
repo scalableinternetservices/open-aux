60eea011c3deca098d8c0c814347f8d86597cf1d
Han <bin315a1@gmail.com>
2019-11-07 14:38:24 -0800

    removed faulty migrations

--- db/migrate/20191107215855_remove_column_from_table.rb
+++
@@ -1,5 +0,0 @@
-class RemoveColumnFromTable < ActiveRecord::Migration[6.0]
-  def change
-    remove_reference :users, :playlists, index: true, foreign_key: true
-  end
-end
--- db/migrate/20191107221739_remove_songs_id_from_playlist.rb
+++
@@ -1,5 +0,0 @@
-class RemoveSongsIdFromPlaylist < ActiveRecord::Migration[6.0]
-  def change
-    remove_reference :playlists, :songs, index: true, foreign_key: true
-  end
-end
