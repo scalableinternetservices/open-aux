31331c4cf82e86e8219220b23c4d22b510957935
Han <bin315a1@gmail.com>
2019-11-25 11:11:23 -0800

    minor fix

--- app/controllers/guests_controller.rb
+++ app/controllers/guests_controller.rb
@@ -2,10 +2,11 @@ require 'bcrypt'
 class GuestsController < ApplicationController
   def new
   end
+  
   def change
     # debugger
     @hashed_id = BCrypt::Password.new(params[:key])
-    session[:playlist_hashed_id] = @hashed_id
-    render 'new'
+    session[:hashed_id] = @hashed_id
+    redirect_to '/dashboard'
   end
 end
