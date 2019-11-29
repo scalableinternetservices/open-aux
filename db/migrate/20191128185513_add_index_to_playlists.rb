1e8dc8146fc276117e740579b09e84f0d1433814
Giovanni Moya <giovmoya@gmail.com>
2019-11-28 16:08:18 -0800

    Revert "added hashed_id as indexes for playlist and playlistSongs"

--- db/migrate/20191128185513_add_index_to_playlists.rb
+++
@@ -1,5 +0,0 @@
-class AddIndexToPlaylists < ActiveRecord::Migration[6.0]
-  def change
-    add_index :playlists, :hashed_id
-  end
-end
--- db/migrate/20191128185724_add_index_to_playlist_song.rb
+++
@@ -1,5 +0,0 @@
-class AddIndexToPlaylistSong < ActiveRecord::Migration[6.0]
-  def change
-    add_index :playlist_songs, :hashed_id
-  end
-end
