492e424d356def3bd75ac7cd49bbae1aab57bf45
Han <bin315a1@gmail.com>
2019-12-06 11:13:41 -0800

    changed database for local; DO NOT PUSH

--- db/schema.rb
+++ db/schema.rb
@@ -31,7 +31,6 @@ ActiveRecord::Schema.define(version: 2019_11_28_185724) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "hashed_id"
     t.integer "user_id"
-    t.integer "userId"
     t.index ["hashed_id"], name: "index_playlists_on_hashed_id"
   end
 
@@ -46,6 +45,7 @@ ActiveRecord::Schema.define(version: 2019_11_28_185724) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
+    t.string "password"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "password_digest"
